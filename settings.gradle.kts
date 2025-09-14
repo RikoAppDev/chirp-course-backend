10b4f2ee96b106e02cec671e610e1a26b597156e
RikoAppDev <riko.appdev@gmail.com>
2025-09-14 20:05:13 +0200

    setup: kotlin common convention plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,14 @@
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
         maven { url = uri("https://repo.spring.io/snapshot") }
         gradlePluginPortal()
     }
 }
 rootProject.name = "ChirpCourse"
+
+include("app")
+include("user")
+include("chat")
+include("notification")
+include("common")
