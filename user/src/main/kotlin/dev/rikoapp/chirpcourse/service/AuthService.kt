fcd2f217bd7ec06e71521e06d29529fbcfba9fb2
RikoAppDev <riko.appdev@gmail.com>
2025-09-22 23:56:52 +0200

    feat (logout): api and auth controller

--- user/src/main/kotlin/dev/rikoapp/chirpcourse/service/AuthService.kt
+++ user/src/main/kotlin/dev/rikoapp/chirpcourse/service/AuthService.kt
@@ -120,6 +120,13 @@ class AuthService(
         } ?: throw UserNotFoundException()
     }
 
+    @Transactional
+    fun logout(refreshToken: String) {
+        val userId = jwtService.getUserIdFromToken(refreshToken)
+        val hashed = hashToken(refreshToken)
+        refreshTokenRepository.deleteByUserIdAndHashedToken(userId, hashed)
+    }
+
     private fun storeRefreshToken(userId: UserId, token: String) {
         val hashedToken = hashToken(token)
         val expiryMs = jwtService.refreshTokenValidityMs
